32fea0b5690d444e70788fbe98dcb48e27a7e64f
Enkei92 <yukhnevich92@gmail.com>
2018-04-24 15:40:20 +0300

    fix:delete code from view

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,9 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable      
-  enum role: {vendor: 0, customer: 1, admin: 2}
+  enum role: { vendor: 0, customer: 1, admin: 2 }
 
+  def self.without_admin
+    Account.roles.except(:admin)
+  end	
 end
