c3e18fe06816dd47f480372a0d4a5ffd2b289ee1
Enkei92 <yukhnevich92@gmail.com>
2018-05-21 17:43:32 +0300

    chore(reek): delete exculdes

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -2,8 +2,12 @@ class AccountMailer < ApplicationMailer
   def send_mail(account, mail_name, reciever)
     @account = account
     @custom_mail = CustomMail.send(mail_name).first
-    return unless @custom_mail.enabled_by_admin?
-    mail(to: reciever, subject: @custom_mail.subject,
-         content_type: 'html/text', body: CustomMailService.interpolate_mail(@account, @custom_mail.mail_type))
+    return unless custom_mail.enabled_by_admin?
+    mail(to: reciever, subject: custom_mail.subject,
+         content_type: 'html/text', body: CustomMailService.interpolate_mail(account, custom_mail.mail_type))
   end
+
+  private
+
+  attr_reader :account, :custom_mail
 end
